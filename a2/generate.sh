b666f7b2faf77dd7cb0e11fcaf62a5e8c5bd6d21
RamaneekGill <ramaneek.gill@hotmail.com>
2015-03-13 20:23:13 -0400

    Adding opt algo to the script

--- a2/generate.sh
+++ a2/generate.sh
@@ -30,15 +30,15 @@ echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
 echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
 ./sim -f traceprogs/tr-blocked.ref -m 200 -s 3000 -a rand | tail -n 7
 echo
-echo "*********fib*********"
+echo "*********my_trace_generator*********"
 echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 50 -s 3000 -a rand | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 50 -s 3000 -a rand | tail -n 7
 echo "!!!!!!!!!Memory size: 100!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 100 -s 3000 -a rand | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 100 -s 3000 -a rand | tail -n 7
 echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 150 -s 3000 -a rand | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 150 -s 3000 -a rand | tail -n 7
 echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 200 -s 3000 -a rand | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 200 -s 3000 -a rand | tail -n 7
 echo "-------------- Results for FIFO --------------"
 echo "*********simpleloop*********"
 echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
@@ -70,15 +70,15 @@ echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
 echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
 ./sim -f traceprogs/tr-blocked.ref -m 200 -s 3000 -a fifo | tail -n 7
 echo
-echo "*********fib*********"
+echo "*********my_trace_generator*********"
 echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 50 -s 3000 -a fifo | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 50 -s 3000 -a fifo | tail -n 7
 echo "!!!!!!!!!Memory size: 100!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 100 -s 3000 -a fifo | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 100 -s 3000 -a fifo | tail -n 7
 echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 150 -s 3000 -a fifo | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 150 -s 3000 -a fifo | tail -n 7
 echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 200 -s 3000 -a fifo | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 200 -s 3000 -a fifo | tail -n 7
 echo "-------------- Results for LRU --------------"
 echo "*********simpleloop*********"
 echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
@@ -110,15 +110,15 @@ echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
 echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
 ./sim -f traceprogs/tr-blocked.ref -m 200 -s 3000 -a lru | tail -n 7
 echo
-echo "*********fib*********"
+echo "*********my_trace_generator*********"
 echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 50 -s 3000 -a lru | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 50 -s 3000 -a lru | tail -n 7
 echo "!!!!!!!!!Memory size: 100!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 100 -s 3000 -a lru | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 100 -s 3000 -a lru | tail -n 7
 echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 150 -s 3000 -a lru | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 150 -s 3000 -a lru | tail -n 7
 echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
-./sim -f traceprogs/tr-fib.ref -m 200 -s 3000 -a lru | tail -n 7
+./sim -f traceprogs/tr-my_trace_generator.ref -m 200 -s 3000 -a lru | tail -n 7
 echo "-------------- Results for clock --------------"
 echo "*********simpleloop*********"
 echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
@@ -159,4 +159,45 @@ echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
 ./sim -f traceprogs/tr-my_trace_generator.ref -m 150 -s 3000 -a clock | tail -n 7
 echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
 ./sim -f traceprogs/tr-my_trace_generator.ref -m 200 -s 3000 -a clock | tail -n 7
+echo "----------------------------------------------"
+echo "-------------- Results for opt --------------"
+echo "*********simpleloop*********"
+echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
+./sim -f traceprogs/tr-simpleloop.ref -m 50 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 100!!!!!!!!!"
+./sim -f traceprogs/tr-simpleloop.ref -m 100 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
+./sim -f traceprogs/tr-simpleloop.ref -m 150 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
+./sim -f traceprogs/tr-simpleloop.ref -m 200 -s 3000 -a opt | tail -n 7
+echo
+echo "*********matmul*********"
+echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
+./sim -f traceprogs/tr-matmul.ref -m 50 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 100!!!!!!!!!"
+./sim -f traceprogs/tr-matmul.ref -m 100 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
+./sim -f traceprogs/tr-matmul.ref -m 150 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
+./sim -f traceprogs/tr-matmul.ref -m 200 -s 3000 -a opt | tail -n 7
+echo
+echo "*********blocked*********"
+echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
+./sim -f traceprogs/tr-blocked.ref -m 50 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 100!!!!!!!!!"
+./sim -f traceprogs/tr-blocked.ref -m 100 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
+./sim -f traceprogs/tr-blocked.ref -m 150 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
+./sim -f traceprogs/tr-blocked.ref -m 200 -s 3000 -a opt | tail -n 7
+echo
+echo "*********my_trace_generator*********"
+echo "!!!!!!!!!Memory size: 50!!!!!!!!!"
+./sim -f traceprogs/tr-my_trace_generator.ref -m 50 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 100!!!!!!!!!"
+./sim -f traceprogs/tr-my_trace_generator.ref -m 100 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 150!!!!!!!!!"
+./sim -f traceprogs/tr-my_trace_generator.ref -m 150 -s 3000 -a opt | tail -n 7
+echo "!!!!!!!!!Memory size: 200!!!!!!!!!"
+./sim -f traceprogs/tr-my_trace_generator.ref -m 200 -s 3000 -a opt | tail -n 7
 echo "----------------------------------------------"
